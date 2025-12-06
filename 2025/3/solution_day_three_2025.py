6e6b406e4a342b2da7a960e1a54eac55a68ca3a6
Max Anderson <Max.A.Anderson95@gmail.com>
2025-12-06 15:36:28 -0500

    finally figured out part two. Re-worked both part one and two to use the same algo

--- 2025/3/solution_day_three_2025.py
+++ 2025/3/solution_day_three_2025.py
@@ -1,4 +1,3 @@
-from itertools import product
 import pathlib
 import sys
 
@@ -8,26 +7,37 @@ def get_input(file_name: str = "input.txt") -> list[str]:
         data = f.readlines()
     return [line.rstrip() for line in data]
 
-
-def solution_part_one(input: str) -> int:
+def solve(input: str, need: int) -> int:
     accum = 0
     for bank in input:
-        highest = 0
-        for i in range(len(bank)):
-            for j in range(i + 1, len(bank)):
-                num = int(bank[i]+bank[j])
-                if num > highest:
-                    highest = num
+        string = ''
+        start = 0
+        found_count = 0
+        while found_count < need:
+            remaining = need - found_count   
+            highest = 0
+            highest_idx = 0
+            for j in range(start, len(bank) - (remaining - 1)):
+                if int(bank[j]) > highest:
+                    highest = int(bank[j])
+                    highest_idx = j
+            
+            string += str(highest)
+            found_count += 1
+            start = highest_idx + 1
 
-        accum += highest
+        accum += int(string)
 
     return accum
 
+def solution_part_one(input: str) -> int:
+    return solve(input, 2)
+
 def solution_part_two(input: str) -> int:
-    pass
+    return solve(input, 12)
 
 
 if __name__ == "__main__":
     input = get_input("input.txt")
     print(solution_part_one(input))
-    # print(solution_part_two(input))
+    print(solution_part_two(input))
